80212f70a5d72fb0364d4e0023e4156d53db547f
shahrukh khan <shahrukh.khan072@gmail.com>
2023-06-03 17:35:34 +0530

    add env

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/guruji_astro'
+mongoose.connect(
+  process.env.MONGO_URL
 ,{
   useNewUrlParser: true,
   useUnifiedTopology: true
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port =  8080;
+const port = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
 const db = require("./config/database");
 const userRoutes = require("./routes/userRoutes");
